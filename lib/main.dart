5d39bfffae4248e0d271fed7a2ed6bdcdb10a989
okeowosv <okeowoonline@gmail.com>
2022-03-02 00:08:59 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:worldclock/pages/home.dart';
+// import 'pages/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}

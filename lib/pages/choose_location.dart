866f1d2dad0e706921653b1ccd913c0957a41083
okeowosv <okeowoonline@gmail.com>
2022-03-05 23:47:54 +0100

    Using the listview builder

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:worldclock/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   ChooseLocation({Key? key}) : super(key: key);
@@ -8,9 +9,19 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    print('Build function ran');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -18,6 +29,25 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: Text('Choose your location'),
         centerTitle: true,
         elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  print(locations[index].location);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                    backgroundImage:
+                        AssetImage('assets/img/${locations[index].flag}')),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
